b3e9d0772060435a8eed7b3e7b71301611cad014
Oleg Suhockiy <suhockiy.o.i@gmail.com>
2015-08-26 19:11:42 +0300

    Project created

---
+++ geoKitty/geoKitty/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  geoKitty
+//
+//  Created by Oleg Suhockiy on 26.08.15.
+//  Copyright (c) 2015 Oleg Suhockiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
